bb08804166fb16aa5186b4735a161d9855f85c26
Nancy Lin <nylin@ualberta.ca>
2025-01-17 15:17:56 -0700

    added comments

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,3 +1,21 @@
+/*
+    CMPUT 301
+
+    Idk about Version information, lets say 0.5 or something
+
+    Today is Jan.17, 2025
+
+    See the license.md file about copyright man
+
+
+    !!!How this works!!!
+    1. Enter the name of the city into the textbox near the bottom, and then click "Add" button
+       below to add the city to the list
+
+    2. Tap a city name to select it then press "Delete" to remove the city from the listview.
+ */
+
+
 package com.example.listycity;
 
 import android.os.Bundle;
@@ -6,10 +24,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -26,9 +46,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
-    StringBuffer cityUserInput = new StringBuffer();
 
+    StringBuffer cityUserInput = new StringBuffer();
     int lastClickedCityPosition = -1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +64,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        //create a List of cities to be displayed
+        //Lab demo code: create a List of cities to be displayed
 
-        //ListView cityList = (ListView) findViewById(R.id.city_list);
         cityList = findViewById(R.id.city_list);
 
         String[] cities = {"Edmonton", "Paris", "London", "Shanghai" };
@@ -55,57 +76,30 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this,R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        //------------------------Lab participation code:-----------------------------------------
 
-        /*
-        Button buttonAdd = (Button) findViewById(R.id.button_add);
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("BUTTONS", "User tapped the Add button");
-            }
-        });
+        
 
-         */
-
-        TextInputEditText cityInput = (TextInputEditText) findViewById(R.id.city_input_edit_text);
-
-
-        Button buttonAdd = (Button) findViewById(R.id.button_add);
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("BUTTONS", "User tapped the Add button");
-
-                cityUserInput.append(cityInput.getText());
-
-                Log.d("BUTTONS", cityUserInput.toString());
-
-                dataList.add(cityUserInput.toString());
-                cityAdapter.notifyDataSetChanged();
-                cityUserInput.setLength(0);
-
-            }
-        });
-
-
-
+        //Section for deleting items in the list
+        //1. Create a listener for the delete button
         Button buttonDelete = (Button) findViewById(R.id.button_delete);
         buttonDelete.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("BUTTONS", "User tapped the Delete button");
+                //Log.d("BUTTONS", "User tapped the Delete button");
+
+                //Only delete the item if its position exists within the size of the list of cities
                 if (lastClickedCityPosition != -1 && lastClickedCityPosition <= cities.length-1){
                     dataList.remove(lastClickedCityPosition);
                     //cityAdapter.remove(String.valueOf(lastClickedCityPosition));
                     cityAdapter.notifyDataSetChanged();
-                    Log.d("BUTTONS", "bruhh" + lastClickedCityPosition);
+                    //Log.d("BUTTONS", "bruhh" + lastClickedCityPosition);
                     lastClickedCityPosition = -1;
                 }
 
             }
         });
-
-
-
+        //2. add listener to the ListView, cityList
         ListView citiesListView = (ListView) cityList;
-
         citiesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
@@ -115,6 +109,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+        //add a listener to the add button
+        TextInputEditText cityInput = (TextInputEditText) findViewById(R.id.city_input_edit_text);
+
+        Button buttonAdd = (Button) findViewById(R.id.button_add);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Log.d("BUTTONS", "User tapped the Add button");
+
+                cityUserInput.append(cityInput.getText());
+
+                //Log.d("BUTTONS", cityUserInput.toString());
+
+                dataList.add(cityUserInput.toString());
+                cityAdapter.notifyDataSetChanged();
+                cityUserInput.setLength(0);
+
+                lastClickedCityPosition = -1;
+
+            }
+        });
 
 
 
